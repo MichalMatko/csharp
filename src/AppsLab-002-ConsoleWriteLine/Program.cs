79d2e1560732ac9839953fc685adadd94df475b6
Michal Maťko <michalmatko44@gmail.com>
2024-09-12 09:17:44 +0200

    .

--- src/AppsLab-002-ConsoleWriteLine/Program.cs
+++ src/AppsLab-002-ConsoleWriteLine/Program.cs
@@ -1,1 +1,10 @@
 ﻿Console.WriteLine("Hello, World!");
+
+string greetings = "Ahoj svet!";
+Console.WriteLine(greetings);
+
+greetings = "Dovidenia svet!";
+Console.WriteLine(greetings);
+
+string name = "Michal!";
+Console.WriteLine("Hello" + name);
