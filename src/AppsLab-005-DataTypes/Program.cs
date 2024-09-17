166b673882caa983d104c89b72aa1d5457ccb6c2
Michal Maťko <michalmatko44@gmail.com>
2024-09-17 08:25:20 +0200

    y&dxycfvgh

--- src/AppsLab-005-DataTypes/Program.cs
+++ src/AppsLab-005-DataTypes/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int age = 16;
+string name = "Michal";
+char inicialky = 'M';
+bool isAdult = false;
+float accountBalance = 0;
+
+Console.WriteLine(name);
+Console.WriteLine(age);
+Console.WriteLine(isAdult);
+Console.WriteLine(accountBalance);
+
