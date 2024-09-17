c55a9f15f78ee9e39503b68548e25b780d32366b
Michal Maťko <michalmatko44@gmail.com>
2024-09-17 09:37:39 +0200

    .

--- src/AppsLab-008-ConsoleReadLine/Program.cs
+++ src/AppsLab-008-ConsoleReadLine/Program.cs
@@ -1,8 +1,12 @@
 ﻿Console.WriteLine("Ako sa voláš?");
 string? meno = Console.ReadLine();
-Console.WriteLine("Ahoj, " + meno);
 
 Console.WriteLine("Koľko máš rokov?");
 string vstup = Console.ReadLine() ?? "0";
 int vek = int.Parse(vstup);
-Console.WriteLine("Máš " + vek + " rokov.");
+Console.WriteLine("Máš " + vek + " rokov." + "\n");
+
+Console.WriteLine("Aké je tvoje obľúbené jedlo?");
+string? jedlo = Console.ReadLine();
+
+Console.WriteLine("Ahoj tvoje meno je " + meno + " máš " + vek + " rokov" + " tvoje obľúbené jedlo je " + jedlo );
