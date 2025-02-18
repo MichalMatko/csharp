ecedf8564121a1bf7ac2194de6c04359771b411e
Michal Maťko <michalmatko44@gmail.com>
2025-02-18 11:04:40 +0100

    .

---
+++ Kniznica/Kniha.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kniznica
+{
+    internal class Kniha
+    {
+        public string Nazov { get; set; }
+        public string Autor { get; set; }
+        public int ISBN { get; set; }
+        public int RokVydania { get; set; }
+        public int PocetDostupnychKusov { get; set; }
+        public KategorieKnih Kategoria { get; set; }
+
+        public Kniha()
+        {
+        }
+
+        public Kniha(string nazov, string autor, int iSBN, int rokVydania, int pocetDostupnychKusov, KategorieKnih kategoria)
+        {
+            Nazov = nazov;
+            Autor = autor;
+            ISBN = iSBN;
+            RokVydania = rokVydania;
+            PocetDostupnychKusov = pocetDostupnychKusov;
+            Kategoria = kategoria;
+        }
+
+        public override string ToString()
+        {
+            return Nazov;
+        }
+    }
+}
---
+++ Kniznica/Pouzivatel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kniznica
+{
+    internal class Pouzivatel
+    {
+        private static int _nextID = 1; //priklad pre automaticke generovanie ID
+        public int ID { get; private set; }
+        public string Meno { get; set; }
+        public string Priezvisko { get; set; }
+        public string Email { get; set; }
+        public int Telefon { get; set; }
+
+        public Pouzivatel(int iD, string meno, string priezvisko, string email, int telefon)
+        {
+            ID = iD;
+            Meno = meno;
+            Priezvisko = priezvisko;
+            Email = email;
+            Telefon = telefon;
+        }
+
+        public override string ToString()
+        {
+            return $"ID: {ID} | {Meno} {Priezvisko}, Email: {Email}, Telefon: {Telefon}";
+        }
+    }
+}
